de99ca7030428906b60bdda2d7696198918fd589
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-08 15:55:34 +0530

    Created Queue

--- Program.cs
+++ Program.cs
@@ -3,19 +3,27 @@
     public class Program
     {
         public static void Main(string[]args)
-        {
-            Console.WriteLine("Pushing values to Stack");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.push(70);//1st in and last in stack
-            linkedListStack.push(30);
-            linkedListStack.push(56);//last in and on top of stack(UC1 requirement)
-            linkedListStack.Display();
+        {   // STACKS  //LIFO
+            //Console.WriteLine("Pushing values to Stack");
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.push(70);//1st in and last in stack
+            //linkedListStack.push(30);
+            //linkedListStack.push(56);//last in and on top of stack(UC1 requirement)
+            //linkedListStack.Display();
 
-            Console.WriteLine("Using Peek and Pop Till Stack is Empty");
-            linkedListStack.Peek();//shows top element of stack
-            linkedListStack.Pop();//deletes top element of stack
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
+            //Console.WriteLine("Using Peek and Pop Till Stack is Empty");
+            //linkedListStack.Peek();//shows top element of stack
+            //linkedListStack.Pop();//deletes top element of stack
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+
+            //QUEUES  //FIFO
+            LinkedListQueue linkedListQueues = new LinkedListQueue();
+            linkedListQueues.Enqueue(56);//1ST in 1st out
+            linkedListQueues.Enqueue(30);
+            linkedListQueues.Enqueue(70);
+            linkedListQueues.Display();
+
         }
     }
 }
